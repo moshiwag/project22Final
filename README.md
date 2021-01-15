939304e207518e936c1a7bb6d14b2a8fabf74248
moshiwag <75214850+moshiwag@users.noreply.github.com>
2021-01-15 21:13:27 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# project22Final
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,43 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var object;
+
+var ground, ball;
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  var ground_options = {
+    isStatic: true
+  }
+
+  var ball_options ={
+    restitution : 0.8
+  }
+
+  ground =  Bodies.rectangle(200, 390, 400, 20,ground_options);
+  World.add(world,ground);
+
+  ball = Bodies.circle(200, 100, 20, ball_options);
+  World.add(world,ball);
+  //console.log(object);
+}
+
+function draw() {
+  background(0);  
+  Engine.update(engine);
+
+  fill("brown");
+  rectMode(CENTER);
+  rect(ground.position.x,ground.position.y, 400, 20);
+  
+  ellipseMode(RADIUS);
+  fill("pink");
+  circle(ball.position.x, ball.position.y, 20);
+}
